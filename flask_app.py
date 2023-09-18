389d7dee000408c22006f5849d3a8df1d1285594
Nini Koning <ninigithub@gmail.com>
2023-09-18 17:26:44 +0200

    tekst aangepast

--- flask_app.py
+++ flask_app.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<p>Hello world!</p>"
+    return "<p>I have the high ground</p>"
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
